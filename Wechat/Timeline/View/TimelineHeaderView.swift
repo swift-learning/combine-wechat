931fd9b9f68bc8ffe5895e1c792e7149f714bd4d
weisite <site.wei@thoughtworks.com>
2022-04-06 17:49:12 +0800

    feat: render timeline background picture from url

--- Wechat/Timeline/View/TimelineHeaderView.swift
+++ Wechat/Timeline/View/TimelineHeaderView.swift
@@ -9,20 +9,25 @@ import SwiftUI
 
 struct TimelineHeaderView: View {
     @EnvironmentObject private var loginedMode: LoginedModelFromHomeView
-
+    
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
-            Image(loginedMode.profile.backgroundUrl)
-                .resizable()
-                .scaledToFill()
+            AsyncImage(url: URL(string: loginedMode.profile.backgroundUrl)) { image in
+                image.resizable()
+            } placeholder: {
+                Color.white
+            }
+            .scaledToFill()
             HStack {
                 Text(loginedMode.profile.nickname)
                     .foregroundColor(.white)
                     .bold()
-                Image(loginedMode.profile.avatarUrl)
-                    .resizable()
-                    .frame(width: 70,
-                           height: 70)
+                AsyncImage(url: URL(string: loginedMode.profile.avatarUrl)) { image in
+                    image.resizable()
+                } placeholder: {
+                    Color.white
+                }
+                .frame(width: 70, height: 70)
             }
             .offset(x: -15, y: 15)
         }
